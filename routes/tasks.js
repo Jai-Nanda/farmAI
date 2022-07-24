bdc177a7426154522bdc8677a31916d4755d58e4
Jai Nanda <85067748+Jai-Nanda@users.noreply.github.com>
2022-07-24 20:35:41 +0530

    changed

--- routes/tasks.js
+++ routes/tasks.js
@@ -13,43 +13,6 @@ router.get("/", (req, res) => {
 });
 
 
-// Update the status of the task using the task id
-router.post("/claim", (req, res) => {
-    supabase.from('orders')
-        .update({status: "inprogress", employee: req.body.employee})
-        .match({id: req.body.id})
-        .then(data => {
-            res.json(data)
-        });
-});
-
-
-router.post('/complete', (req, res) => {
-    const date = new Date()
-    const today = String(date.getFullYear()) + '-' + String(date.getMonth() + 1) + '-' + String(date.getDate())
-
-    supabase.from('orders')
-        .update({status: "completed", complete_date: today})
-        .match({id: req.body.id})
-        .then(data => {
-            supabase.from('inventory').select().match({item: data.data[0].flavour})
-            .then(result => {
-                supabase.from('inventory').update({quantity: result.data[0].quantity - data.data[0].quantity}).match({item: data.data[0].flavour})
-                .then(re => {
-                    const post = http.request({
-                        host: 'localhost',
-                        port: '8000',
-                        path: '/reward/give',
-                        method: 'POST',
-                        headers: {order_id: req.body.id}
-                    })
-        
-                    post.end()
-        
-                    res.json(data);
-                })
-            })
-        })
-});
+router.post('/')
 
 module.exports = router
